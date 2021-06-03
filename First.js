fdbdd8a64d5ad09e31df0593598d1d6b21fedda6
shilpa ajeesh <69710142+shilpaajeeshm@users.noreply.github.com>
2021-06-03 18:45:39 +0530

    Add files via upload

---
+++ First.js
@@ -0,0 +1,8 @@
+var a = 20;
+var b = 30;
+if(a>b){
+    console.log("a is large")
+}
+else{
+    console.log("b is large")
+}
---
+++ second.js
@@ -0,0 +1,6 @@
+var a = 5;
+i=0;
+while(i<a+1){
+    console.log(i);
+    i++;
+}
