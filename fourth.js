cf9ff26df0631a960c2b9033e7e547ad267d5797
shilpa ajeesh <69710142+shilpaajeeshm@users.noreply.github.com>
2021-06-04 16:23:16 +0530

    Add files via upload

---
+++ fourth.js
@@ -0,0 +1,10 @@
+var fruit="apple";
+if(fruit=="apple"){
+    console.log("apple")
+}else if(fruit=="grapes"){
+    console.log("grapes")
+}else if(fruit=="orange"){
+    console.log("orange")
+}else{
+    console.log("not a fruit")
+}
---
+++ sixth.js
@@ -0,0 +1,8 @@
+var fruit="grape"
+switch(fruit){
+    case "grape":console.log("grape")
+        break;
+    case "apple":console.log("apple")
+        break;
+    default:console.log("none of these")
+}
