c58c5851eeef55171f94ccf07678ca627ae29e3c
shilpa ajeesh <69710142+shilpaajeeshm@users.noreply.github.com>
2021-06-16 13:31:25 +0530

    Add files via upload

---
+++ basename.js
@@ -0,0 +1,3 @@
+var path=require("path");
+console.log(path.basename(__dirname));
+console.log(path.basename(__filename));
---
+++ extname.js
@@ -0,0 +1,3 @@
+var path=require("path");
+console.log(path.extname(__dirname));
+console.log(path.extname(__filename));
